cd94c70f3c64a473b98e9109453a91d7c64ca39e
Ryans Ren <ryansren@bestaxc.com.cn>
2021-08-05 18:36:27 +0800

    commit

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,68 +1,17 @@
 //index.js
+// import moment from 'moment'
 const app = getApp()
 const { envList } = require('../../envList.js')
+const moment = require('moment')
+const startDate = moment('2020-10-5')
 
 Page({
   data: {
-    showUploadTip: false,
-    powerList: [{
-      title: '云函数',
-      tip: '安全、免鉴权运行业务代码',
-      showItem: false,
-      item: [{
-        title: '获取OpenId',
-        page: 'getOpenId'
-      },
-      //  {
-      //   title: '微信支付'
-      // },
-       {
-        title: '生成小程序码',
-        page: 'getMiniProgramCode'
-      },
-      // {
-      //   title: '发送订阅消息',
-      // }
-    ]
-    }, {
-      title: '数据库',
-      tip: '安全稳定的文档型数据库',
-      showItem: false,
-      item: [{
-        title: '创建集合',
-        page: 'createCollection'
-      }, {
-        title: '更新记录',
-        page: 'updateRecord'
-      }, {
-        title: '查询记录',
-        page: 'selectRecord'
-      }, {
-        title: '聚合操作',
-        page: 'sumRecord'
-      }]
-    }, {
-      title: '云存储',
-      tip: '自带CDN加速文件存储',
-      showItem: false,
-      item: [{
-        title: '上传文件',
-        page: 'uploadFile'
-      }]
-    }, {
-      title: '云托管',
-      tip: '不限语言的全托管容器服务',
-      showItem: false,
-      item: [{
-        title: '部署服务',
-        page: 'deployService'
-      }]
-    }],
-    envList,
-    selectedEnv: envList[0],
-    haveCreateCollection: false
+    date: '',
   },
-
+  onLoad: function () {
+    this.setData({date: moment().diff(startDate, 'days')})
+  },
   onClickPowerInfo(e) {
     const index = e.currentTarget.dataset.index
     const powerList = this.data.powerList
